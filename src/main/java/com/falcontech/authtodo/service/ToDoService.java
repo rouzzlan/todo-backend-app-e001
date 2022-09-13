57dd1e15614b38807b698489d7ef8196ffd47049
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2022-09-13 22:55:23 +0300

    dummy values added

--- src/main/java/com/falcontech/authtodo/service/ToDoService.java
+++ src/main/java/com/falcontech/authtodo/service/ToDoService.java
@@ -9,7 +9,17 @@ import java.util.List;
 
 @Service
 public class ToDoService {
-  private List<ToDo> todos = new ArrayList<>();
+  private final List<ToDo> todos;
+
+  public ToDoService() {
+    todos = new ArrayList<>();
+    ToDo toDo = new ToDo();
+    toDo.setId("387ddff8-e0ad-44e5-a7a9-ffc6d9866669");
+    toDo.setTitle("Test");
+    toDo.setText("lorem ipsum");
+    toDo.setWriterName("John Do");
+    todos.add(toDo);
+  }
 
   public ImmutableList<ToDo> getToDos() {
     return ImmutableList.copyOf(todos);
